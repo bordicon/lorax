33115c2b88c138ee26380a37f418e752584f4ce8
jint <jesse@nanosatisfi.com>
2015-02-21 19:36:30 -0800

    Add installation tools in form of setup.py file and install.sh executable

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+# Bash safe mode
+set -e
+set -u
+set -o pipefail
+set -o verbose
+
+# Reference everything relative to directory install.sh script is in.
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Installing..."
+cd $script_dir
+sudo python ./setup.py install || { echo "ERROR: Installing comms library"; exit 1; }
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+setup(
+	name='comms',
+	version='0.1',
+	author="Jesse Trutna",
+	author_email="jesse@spire.com",
+	py_modules=['comms']
+	)
+
